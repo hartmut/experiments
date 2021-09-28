6095755a4fb9e2e216969193df42845fc14a07b7
Hartmut Prochaska <hartmut.prochaska@gmail.com>
2021-09-28 21:01:28 +0200

    tick player

--- rltk/src/main.rs
+++ rltk/src/main.rs
@@ -33,6 +33,9 @@ impl<'a> System<'a> for LeftWalker {
 
 }
 
+#[derive(Component, Debug)]
+struct Player {}
+
 struct State {
     ecs: World,
 }
@@ -41,6 +44,7 @@ impl GameState for State {
     fn tick(&mut self, ctx : &mut Rltk) {
         ctx.cls();
 
+        player_input(self, ctx);
         self.run_systems();
 
         let positions = self.ecs.read_storage::<Position>();
@@ -61,6 +65,30 @@ impl State {
 
 }
 
+fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
+    let mut positions = ecs.write_storage::<Position>();
+    let mut players = ecs.write_storage::<Player>();
+
+    for (_player, pos) in (&mut players, &mut positions).join() {
+        pos.x = min(79 , max(0, pos.x + delta_x));
+        pos.y = min(49, max(0, pos.y + delta_y));
+    }
+}
+
+fn player_input(gs: &mut State, ctx: &mut Rltk) {
+    // Player movement
+    match ctx.key {
+        None => {} // Nothing happened
+        Some(key) => match key {
+            VirtualKeyCode::Left => try_move_player(-1, 0, &mut gs.ecs),
+            VirtualKeyCode::Right => try_move_player(1, 0, &mut gs.ecs),
+            VirtualKeyCode::Up => try_move_player(0, -1, &mut gs.ecs),
+            VirtualKeyCode::Down => try_move_player(0, 1, &mut gs.ecs),
+            _ => {}
+        },
+    }
+}
+
 fn main() -> rltk::BError {
 
     use rltk::RltkBuilder;
@@ -74,8 +102,9 @@ fn main() -> rltk::BError {
 
     gs.ecs.register::<Position>();
     gs.ecs.register::<Renderable>();
-    gs.ecs.register::<LeftMover>();
-
+    gs.ecs.register::<LeftMover>(); 
+    gs.ecs.register::<Player>();
+ 
     gs.ecs
         .create_entity()
         .with(Position { x: 40, y: 25 })
@@ -84,6 +113,7 @@ fn main() -> rltk::BError {
             fg: RGB::named(rltk::YELLOW),
             bg: RGB::named(rltk::BLACK),
         })
+        .with(Player{})
         .build();
 
     for i in 0..10 {
