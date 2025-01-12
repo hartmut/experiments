e28034daac72733aa1605b1e81c3ced3786f1360
Hartmut Prochaska <hartmut.prochaska@gmail.com>
2025-01-12 16:28:59 +0100

    refactor camera setup: increase cylinder height, adjust colors, and reposition camera

--- bevy-torus/src/core/systems/startup/camera.rs
+++ bevy-torus/src/core/systems/startup/camera.rs
@@ -1,5 +1,6 @@
 use bevy::prelude::*;
 use bevy_panorbit_camera::*;
+use std::f32::consts::PI;
 
 #[derive(Component)]
 struct Camera;
@@ -9,30 +10,31 @@ pub fn setup_camera(
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
+    // https://bevy-cheatbook.github.io/fundamentals/coords.html?highlight=handed#2d-and-3d-scenes-and-cameras
     // X
     commands.spawn((
-        Mesh3d(meshes.add(Cylinder::new(0.5, 50.0))),
-        MeshMaterial3d(materials.add(Color::srgb(0.1, 1.0, 0.1))),
-        Transform::from_xyz(25.0, 0.0, 0.0),
+        Mesh3d(meshes.add(Cylinder::new(0.5, 200.0))),
+        MeshMaterial3d(materials.add(Color::srgb_u8(255, 0, 0))),
+        Transform::from_xyz(0.0, 0.0, 0.0).with_rotation(Quat::from_rotation_z(PI * 0.5)),
     ));
+    // TODO let it glow
     // Y
     commands.spawn((
-        Mesh3d(meshes.add(Cylinder::new(0.5, 50.0))),
-        MeshMaterial3d(materials.add(Color::srgb_u8(255, 0, 0))),
+        Mesh3d(meshes.add(Cylinder::new(0.5, 200.0))),
+        MeshMaterial3d(materials.add(Color::srgb_u8(0, 255, 0))),
     ));
     // Z
     commands.spawn((
-        Mesh3d(meshes.add(Cylinder::new(0.5, 50.0))),
+        Mesh3d(meshes.add(Cylinder::new(0.5, 200.0))),
         MeshMaterial3d(materials.add(Color::srgb_u8(0, 0, 255))),
-        Transform::from_xyz(0.0, 0.0, 25.0),
+        Transform::from_xyz(0.0, 0.0, 0.0).with_rotation(Quat::from_rotation_x(PI * 0.5)),
     ));
     // Plane
     commands.spawn((
-        Mesh3d(meshes.add(Plane3d::default().mesh().size(100.0, 100.0))),
-        MeshMaterial3d(materials.add(Color::WHITE)),
+        Mesh3d(meshes.add(Plane3d::default().mesh().size(100.0, 100.0).normal(Dir3::Y))),
+        MeshMaterial3d(materials.add(Color::srgba_u8(255, 255, 255, 64))),
     ));
-    
-    
+
     // light
     commands.spawn((
         PointLight {
@@ -42,13 +44,8 @@ pub fn setup_camera(
         },
         Transform::from_xyz(40.0, 40.0, 100.0),
     ));
-
-    // commands.spawn((
-    //     Camera3d::default(),
-    //     Transform::from_xyz(80.0, 10.0, 80.0).looking_at(Vec3::ZERO, Vec3::Y),
-    // ));
     commands.spawn((
-        Transform::from_translation(Vec3::new(80.0, 10.0, 80.0)),
+        Transform::from_translation(Vec3::new(40.0, 40.0, 80.0)),
         PanOrbitCamera::default(),
     ));
 }
